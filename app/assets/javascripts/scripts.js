0b7048ffb593c4833a57a12760163a265b395c9c
wlst <wilson.lin27@gmail.com>
2014-04-19 22:18:59 -0700

    slow scroll

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -11,5 +11,18 @@ $( document ).ready(function() {
     $("#credit-cards").removeClass();
     $("#credit-cards").addClass($.payment.cardType($("#card_number").val()));
   });
-
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
 });
